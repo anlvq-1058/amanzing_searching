6812a3e624140813985aff02501806557c4d2fb0
anlvq-1058 <le.van.quang.an@sun-asterisk.com>
2022-10-25 18:31:13 +0700

    add view

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import SearchBar from './searchBar';
+import SideBar from './sideBar';
+import ProductTable from './productTable';
+import 'bootstrap/dist/css/bootstrap.css';
+
+require('bootstrap');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <SearchBar />
+    <main>
+      <SideBar />
+      <ProductTable />
+    </main>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
